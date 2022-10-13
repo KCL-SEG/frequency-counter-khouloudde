11dfebc2235423ae0a73f59fb22695fb8c55c9ac
khouloudde <khouloudelaktb@gmail.com>
2022-10-13 16:38:00 +0100

    Count frequencies in list

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,26 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
-    frequencies = {}
-    # Your code goes here
+def frequencies(list):
+    frequencies= {}
+
+    if len(list) == 0:
+        print("Empty list")
+    else:
+
+        items =[str(x) for x in list]
+        items.sort()
+        current_state = items[0]
+        counter = 0
+
+        for i in range(0,len(items)):
+            if items[i] == current_state:
+                counter+=1
+            else:
+                frequencies[current_state] = counter
+                current_state = items[i]
+                counter = 1
+
+            frequencies[current_state] = counter
+
     return frequencies
